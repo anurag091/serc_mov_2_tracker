188312c395deebceb77b3cc96a3aec33f8a43533
System Administrator <root@Anurags-MacBook-Pro.local>
2020-05-01 20:59:42 +0530

    Updates

---
+++ precache-manifest.8201bf71c065fb267cc37b104f301a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac0fa33c032310b13fb10215c5930c2",
+    "url": "/serc_mov_2_tracker/index.html"
+  },
+  {
+    "revision": "dd17f806ff9ff5259ba6",
+    "url": "/serc_mov_2_tracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "4794f386ce9d85a86c49",
+    "url": "/serc_mov_2_tracker/static/js/2.23896539.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/serc_mov_2_tracker/static/js/2.23896539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd17f806ff9ff5259ba6",
+    "url": "/serc_mov_2_tracker/static/js/main.0a181b2d.chunk.js"
+  },
+  {
+    "revision": "14ca0d85ab412e6e958a",
+    "url": "/serc_mov_2_tracker/static/js/runtime-main.36e967ce.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/serc_mov_2_tracker/static/media/image.d7265326.png"
+  }
+]);
